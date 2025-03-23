2c99b1753b0555cfea34ac0fb33327eea3fa2834
Christian Westrom <c.westrom@westrom.xyz>
2025-03-23 14:23:08 +0900

    use nixpkgs instead

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Rust flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     rust-overlay.url = "github:oxalica/rust-overlay";
     devenv.url = "github:cachix/devenv";
     systems.url = "github:nix-systems/default";
